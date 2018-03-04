6c9afc492e2561abb1df177fbaeb85a54deee81f
michellejames <shellyjames90@yahoo.com>
2018-03-04 16:46:04 -0500

    Working on Desktop Version

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,50 @@
-let grid = document.querySelector('.wrapper');
-let masonry = new Masonry( grid, {
-  // options
-  itemSelector: '.wrapper',
-  columnWidth: 200
+// let grid = document.querySelector('.wrapper');
+// let masonry = new Masonry( grid, {
+//   // options
+//   itemSelector: '.wrapper',
+//   columnWidth: 200
+// });
+
+// // element argument can be a selector string
+// //   for an individual element
+// let masonry2 = new Masonry( grid, {
+//  	itemSelector: '.fourth-image',
+//  	columnWidth: 200
+// });
+
+
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 160,
+  stagger: 30,
 });
 
-// element argument can be a selector string
-//   for an individual element
-let masonry = new Masonry( '.grid', {
-  // options
+$grid.on( 'click', '.grid-item', function() {
+  // change size of item via class
+  $( this ).toggleClass('grid-item--gigante');
+  // trigger layout
+  $grid.masonry();
+});
+
+$(".counter").each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+    duration: 3000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
 });
